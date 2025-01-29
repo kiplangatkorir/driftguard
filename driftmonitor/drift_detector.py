818438da74ed679d78e12d34fc7191982a884dbc
Kiplangat Korir <korirkiplangat22@gmail.com>
2025-01-29 14:11:25 +0300

    New

--- driftmonitor/drift_detector.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-def load_data(file_path):
-    """
-    Loads data from a given CSV file.
-    :param file_path: Path to the CSV file.
-    :return: A Pandas DataFrame containing the data.
-    """
-    return pd.read_csv(file_path)
