d28c15f1d1bb261948af6b59b999cd5d9a836355
Kiplangat Korir <korirkiplangat22@gmail.com>
2025-02-06 16:39:23 +0300

    New

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="driftguard",  
-    version="0.1.3",  
+    version="0.1.4",  
     author="Kiplangat Korir",
     author_email="korirkiplangat22@gmail.com",
     description="A lightweight Python library for monitoring data and concept drift in machine learning models.",
