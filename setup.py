9616e7c34e5188c3f8e4ddfda05b285525061bd9
Kiplangat Korir <153384040+kiplangatkorir@users.noreply.github.com>
2026-01-22 12:05:18 +0300

    Update version from 0.1.5 to 0.1.6 in setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,11 @@
-version = '0.1.6'
+from setuptools import setup, find_packages
+
+setup(
+    name='driftguard',
+    version='0.1.6',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'pandas',
+    ],
+)
